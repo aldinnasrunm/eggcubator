75a29c32497878a4dcb47c0cbac5bde169841623
sirupmarjan <sirupmarjanasli@gmail.com>
2021-02-09 13:09:00 +0700

    remove ActionBar in OnBoardingActivity.kt

--- app/src/main/java/com/alden/eggincubator/Activity/OnBoardingActivity.kt
+++ app/src/main/java/com/alden/eggincubator/Activity/OnBoardingActivity.kt
@@ -3,10 +3,14 @@ package com.alden.eggincubator.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alden.eggincubator.R
+import com.alden.eggincubator.databinding.ActivityOnBoardingBinding
 
 class OnBoardingActivity : AppCompatActivity() {
+    lateinit var binding: ActivityOnBoardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
     }
 }
