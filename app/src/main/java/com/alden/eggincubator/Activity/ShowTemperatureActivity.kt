b968d6b5de9a9d2f5dbca203c66e91c3a687bc2a
aldin-nasrun <adnzain40@gmail.com>
2021-02-12 12:42:19 +0700

    ganti cara ambil data dengan data class dan ganti layout ke full screen

--- app/src/main/java/com/alden/eggincubator/Activity/ShowTemperatureActivity.kt
+++ app/src/main/java/com/alden/eggincubator/Activity/ShowTemperatureActivity.kt
@@ -1,10 +1,12 @@
 package com.alden.eggincubator.Activity
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.alden.eggincubator.databinding.ActivityShowTemperatureBinding
+import com.alden.eggincubator.models.RTDBDataClass
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -27,38 +29,38 @@ class ShowTemperatureActivity : AppCompatActivity() {
          * animation link https://lottiefiles.com/29847-owl-breaking-egg
          * https://lottiefiles.com/27777-chiki-chik
          *
-         * list ToDo : tambahkan shimmer
+         * list ToDo : tambahkan shimmer -> done
          *
          * */
-
+        theData()
         val refHum = database.getReference("FirebaseIOT").child("humidity")
         val refTemp = database.getReference("FirebaseIOT").child("temperature")
 
-        refHum.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                var hum = snapshot.value.toString()
-                binding.tvHumidity.text = "$hum %"
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.w(TAG, "Failed to read value.", error.toException())
-            }
-
-        })
-
-        refTemp.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                var temp = snapshot.value.toString()
-                binding.tvTemperature.text = "$temp deg"
-                initAnimation()
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.w(TAG, "Failed to read value.", error.toException())
-
-            }
-
-        })
+//        refHum.addValueEventListener(object : ValueEventListener{
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                var hum = snapshot.value.toString()
+//                binding.tvHumidity.text = "$hum %"
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                Log.w(TAG, "Failed to read value.", error.toException())
+//            }
+//
+//        })
+//
+//        refTemp.addValueEventListener(object : ValueEventListener{
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                var temp = snapshot.value.toString()
+//                binding.tvTemperature.text = "$temp deg"
+//                initAnimation()
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                Log.w(TAG, "Failed to read value.", error.toException())
+//
+//            }
+//
+//        })
     }
 
     private fun initAnimation() {
@@ -70,6 +72,39 @@ class ShowTemperatureActivity : AppCompatActivity() {
             stopShimmer()
             visibility = View.GONE
         }
+    }
+
+
+    private fun theData(){
+        val ref = database.getReference("FirebaseIOT")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+//                Log.d(TAG, "onDataChange: " + snapshot.value)
+                var dataH = snapshot.child("humidity").value.toString()
+                var dataT = snapshot.child("temperature").value.toString()
+                var dataL = snapshot.child("led").value.toString()
+
+                var rtdbData = RTDBDataClass(
+                    dataT,
+                    dataH,
+                    dataL
+                )
+                Log.d(TAG, "onDataChange: $rtdbData")
+
+                inidView(rtdbData)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "onCancelled: ", error.toException())
+            }
+
+        })
+    }
+
+    private fun inidView(rtdbDataClass: RTDBDataClass){
+        initAnimation()
+        binding.tvTemperature.text = rtdbDataClass.temperature + " \u2103"
+        binding.tvHumidity.text = rtdbDataClass.humidity + "%"
     }
 
     override fun onBackPressed() {
