04b64c050060c51fb42c5067849a5c0b3d399137
sirupmarjan <sirupmarjanasli@gmail.com>
2021-02-15 13:50:54 +0700

    change icon color in the tabLayout programatically

--- app/src/main/java/com/alden/eggincubator/Activity/ParentActivity.kt
+++ app/src/main/java/com/alden/eggincubator/Activity/ParentActivity.kt
@@ -1,21 +1,25 @@
 package com.alden.eggincubator.Activity
 
+import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.alden.eggincubator.R
 import com.alden.eggincubator.adapter.ParentAdapter
 import com.alden.eggincubator.databinding.ActivityParentBinding
 import com.alden.eggincubator.fragments.DashboardFragment
 import com.alden.eggincubator.fragments.SettingFragment
 import com.alden.eggincubator.models.CompleteTriggerData
+import com.google.android.material.tabs.TabLayout
 
 
+@Suppress("DEPRECATION")
 class ParentActivity : AppCompatActivity() {
     lateinit var vLivedata : CompleteTriggerData
     lateinit var binding : ActivityParentBinding
     private val tabIcons = intArrayOf(
-        R.drawable.ic_logo_power,
-        R.drawable.ic_logo_reboot
+        R.drawable.ic_gray_home,
+        R.drawable.ic_gray_setting
     )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +34,29 @@ class ParentActivity : AppCompatActivity() {
             offscreenPageLimit = 1
         }
         binding.tabActionList.setupWithViewPager(binding.vpList)
+        binding.tabActionList.tabRippleColor = null
         binding.tabActionList.getTabAt(0)?.setIcon(tabIcons[0])
         binding.tabActionList.getTabAt(1)?.setIcon(tabIcons[1])
-//        vLivedata.status.observe()
+        binding.tabActionList.setOnTabSelectedListener(
+            object : TabLayout.ViewPagerOnTabSelectedListener(binding.vpList) {
+                override fun onTabSelected(tab: TabLayout.Tab) {
+                    super.onTabSelected(tab)
+                    val tabIconColor = ContextCompat.getColor(this@ParentActivity, R.color.greenMain)
+                    tab.icon!!.setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN)
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab) {
+                    super.onTabUnselected(tab)
+                    val tabIconColor =
+                        ContextCompat.getColor(this@ParentActivity, R.color.colorGrey)
+                    tab.icon!!.setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN)
+                }
+
+                override fun onTabReselected(tab: TabLayout.Tab) {
+                    super.onTabReselected(tab)
+                }
+            }
+        )
 
     }
 
