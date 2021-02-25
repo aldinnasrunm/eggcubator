8cbbd740765bd7ef146813c601e97a6c62957bfb
sirupmarjan <sirupmarjanasli@gmail.com>
2021-02-25 11:01:09 +0700

    tambah fungsi update tanggal di StartActivity.kt

--- app/src/main/java/com/alden/eggincubator/Activity/StartActivity.kt
+++ app/src/main/java/com/alden/eggincubator/Activity/StartActivity.kt
@@ -1,21 +1,26 @@
 package com.alden.eggincubator.Activity
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.alden.eggincubator.R
+import androidx.annotation.RequiresApi
 import com.alden.eggincubator.databinding.ActivityStartBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.time.LocalDateTime
 
 class StartActivity : AppCompatActivity() {
     lateinit var binding: ActivityStartBinding
-    val fbdo = FirebaseDatabase.getInstance()
+    val fbdb = FirebaseDatabase.getInstance()
     lateinit var eggEvenListener : ValueEventListener
-    val eggRef = fbdo.getReference("EggData")
+    val eggRef = fbdb.getReference("EggData")
+    val waterInterfal :Long = 4
+    val ref = fbdb.getReference("FirebaseIOT")
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStartBinding.inflate(layoutInflater)
@@ -23,10 +28,18 @@ class StartActivity : AppCompatActivity() {
         isAnimationVisible(true)
         initDay()
         binding.btnStartNext.setOnClickListener {
+            updateMightyDay()
             startActivity(Intent(this, ParentActivity::class.java))
             finish()
         }
 
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun updateMightyDay() {
+        val current = LocalDateTime.now()
+        ref.child("mightyDay").setValue(current.toString())
+        ref.child("waterDay").setValue(current.plusDays(waterInterfal).toString())
     }
 
     private fun initDay() {
